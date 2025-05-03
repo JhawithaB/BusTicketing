b53e06e643cc60e447770a26bcdd734a17bc3ee0
JhawithaB <beltranjhanna8@gmail.com>
2025-05-03 19:34:37 +0800

    Add files via upload

---
+++ Bus Ticketing (2)/Bus Ticketing/Bus Ticketing/src/bus/ticketing/BusTicketing.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bus.ticketing;
+
+/**
+ *
+ * @author user
+ */
+public class BusTicketing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       LOgIn login = new LOgIn();
+         login.setVisible(true);
+         login.show();
+         login.setLocationRelativeTo(null);
+    }
+    
+}
